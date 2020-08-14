9ba4c45e34d78c40ab1210691a72e4e9620ff8f8
Jacob W Jones <jacobwadejones76@gmail.com>
2020-08-14 19:49:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,11 +4,15 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    
- end
+    if number < 1
+      puts"HAPPY NEW YEAR!"
+    end
+  end
 end
 
 countdown(number)
-puts "HAPPY NEW YEAR!"
+
+
+
   
     
